21d430311e6a0c2bec1cddb6dfd7b88b7e2fb894
jktakeshi <jktakeshi@gmail.com>
2025-03-16 12:18:21 -0400

    made slight edits to html files and script.js

--- tkoey-lab3/static/script.js
+++ tkoey-lab3/static/script.js
@@ -30,34 +30,33 @@ else {
 let L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
 let L2 = ["Apple", "Banana", "Kiwi", "Orange"];
 
-function findTheBanana(array) {
+function findTheBanana(array, array_name) {
     array.forEach(item => {
         if (item === "Banana") {
-            alert("Banana found");
+            alert("We found a banana in the " + array_name);
         }
     });
 }
-findTheBanana(L1);
-findTheBanana(L2);
+findTheBanana(L1, "first array");
+findTheBanana(L2, "second array");
 
 function greetingFunc() {
     let d = new Date();
     let h = d.getHours();
     let greeting;
 
-    if (h<12 && h>=5) {
+    if (5<h && h<12) {
         greeting = "Good morning";
     }
-    else if (12<h && h<18) {
+    else if (12<=h && h<18) {
         greeting = "Good afternoon";
     }
-    else if (18<h && h<20) {
+    else if (18<=h && h<20) {
         greeting = "Good evening";
     }
     else {
         greeting = "Good night";
     }
-
     let E = document.getElementById("greeting");
     E.innerHTML = greeting + ", I am Takeshi Koey";
 }
